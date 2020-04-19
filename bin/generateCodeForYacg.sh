daf3bcaad317fbfd6cfce31ba86e0043f62ef428
Okie Othsam <okie.othsam@gmail.com>
2020-04-19 23:15:46 +0200

    wip: create config objects from command line arguments

--- bin/generateCodeForYacg.sh
+++ bin/generateCodeForYacg.sh
@@ -10,12 +10,9 @@ pushd $scriptPos/.. > /dev/null
 echo "create meta model classes ..."
 if ! pipenv run python3 yacg.py --models \
     resources/models/json/yacg_model_schema.json \
-    --output ${scriptPos}/../yacg/model/model.py \
-             ${scriptPos}/../tests/model/test_model.py \
-             ${scriptPos}/../docs/puml/yacg_model.puml \
-    --templates pythonBeans \
-                pythonBeansTests \
-                plantUml \
+    --singleFileTemplates pythonBeans=${scriptPos}/../yacg/model/model.py \
+                pythonBeansTests=${scriptPos}/../tests/model/test_model.py \
+                plantUml=${scriptPos}/../docs/puml/yacg_model.puml \
     --templateParameters modelPackage=yacg.model.model \
                          title="yacg model"; then
     echo "    ERROR while create meta model classes"
@@ -25,12 +22,9 @@ fi
 echo "create config model classes ..."
 if ! pipenv run python3 yacg.py --models \
     resources/models/json/yacg_config_schema.json \
-    --output ${scriptPos}/../yacg/model/config.py \
-             ${scriptPos}/../tests/model/test_config.py \
-             ${scriptPos}/../docs/puml/yacg_config_schema.puml \
-    --templates pythonBeans \
-                pythonBeansTests \
-                plantUml \
+    --singleFileTemplates pythonBeans=${scriptPos}/../yacg/model/config.py \
+                pythonBeansTests=${scriptPos}/../tests/model/test_config.py \
+                plantUml=${scriptPos}/../docs/puml/yacg_config_schema.puml \
     --templateParameters modelPackage=yacg.model.config \
                          title="yacg configuration model"; then
     echo "    ERROR while create config model classes"
@@ -40,12 +34,9 @@ fi
 echo "create openapi model classes ..."
 if ! pipenv run python3 yacg.py --models \
     resources/models/json/yacg_openapi_paths.json \
-    --output ${scriptPos}/../yacg/model/openapi.py \
-             ${scriptPos}/../tests/model/test_openapi.py \
-             ${scriptPos}/../docs/puml/yacg_openapi.puml \
-    --templates pythonBeans \
-                pythonBeansTests \
-                plantUml \
+    --singleFileTemplates pythonBeans=${scriptPos}/../yacg/model/openapi.py \
+                pythonBeansTests=${scriptPos}/../tests/model/test_openapi.py \
+                plantUml=${scriptPos}/../docs/puml/yacg_openapi.puml \
     --templateParameters modelPackage=yacg.model.openapi \
                          title="yacg openapi model"; then
     echo "    ERROR while create openapi model classes"
--- yacg.py
+++ yacg.py
@@ -10,6 +10,9 @@ from yacg.builder.yamlBuilder import getModelFromYaml
 from yacg.generators.singleFileGenerator import renderSingleFileTemplate
 from yacg.util.fileUtils import getInternalTemplatePath
 
+import yacg.model.config as config
+
+
 description = """Yet another code generation.
 Program takes one or more models, a bunch of templates and generates
 source code from it
@@ -22,9 +25,8 @@ parser.add_argument_group('input')
 parser.add_argument('--models', nargs='+', help='models to process')
 parser.add_argument('--config', nargs='?', help='config file')
 parser.add_argument_group('processing')
-parser.add_argument('--templates', nargs='+', help='templates to process')
-parser.add_argument_group('output')
-parser.add_argument('--output', nargs='+', help='output files or directories in order of the given templates')
+parser.add_argument('--singleFileTemplates', nargs='+', help='templates to process that creates one file')
+parser.add_argument('--multiFileTemplates', nargs='+', help='templates to process that creates one file per type')
 parser.add_argument_group('additional')
 parser.add_argument('--templateParameters', nargs='+', help='additional parameters passed to the templates')
 
@@ -36,50 +38,12 @@ def getFileExt(fileName):
     return fileName[lastDot:]
 
 
-def checkTemplatesToUse(args):
-    """test if the required model are there"""
-
-    # extend internal models with the full path
-    fixedTemplates = []
-    for template in args.templates:
-        internalTemplateName = 'yacg/generators/templates/{}.mako'.format(template)
-        isInternalTemplate = doesFileExist(internalTemplateName)
-        if isInternalTemplate is True:
-            fixedTemplates.append(internalTemplateName)
-        else:
-            fixedTemplates.append(template)
-
-    return _checkFilesToUse(fixedTemplates, 'templates')
-
-
-def checkModelsToLoad(args):
-    """test if the desired model files exist"""
-
-    return _checkFilesToUse(args.models, 'models')
-
-
-def _checkFilesToUse(fileList, fileType):
-    if not fileList:
-        print('no {} given, cancel'.format(fileType))
-        return False
-    print('\n{} to use:'.format(fileType))
-    foundAll = True
-    for file in fileList:
-        fileExists = doesFileExist(file)
-        fileExistsString = getOkTxt('found') if fileExists \
-            else getErrorTxt('missing')
-        if not fileExists:
-            foundAll = False
-        print(' {}\t{}'.format(fileExistsString, file))
-    return foundAll
-
-
-def readModels(args):
+def readModels(configJob):
     """reads all desired models and build the model object tree from it"""
 
     loadedTypes = []
     yamlExtensions = set(['.yaml', '.yml'])
-    for model in args.models:
+    for model in configJob.models:
         fileExt = getFileExt(model)
         if fileExt.lower() in yamlExtensions:
             loadedTypes = getModelFromYaml(model, loadedTypes)
@@ -88,7 +52,7 @@ def readModels(args):
     return loadedTypes
 
 
-def getTemplateParameters(args):
+def _getTemplateParameters(args):
     """extracts the per command line given template parameters, copies them
     into a dictionary and return this dictonary
     """
@@ -103,30 +67,157 @@ def getTemplateParameters(args):
     return templateParameters
 
 
+def _getJobConfigurationsFromConfigFile(configFile):
+    # TODO
+    return []
+
+
+def _splitTemplateAndDestination(templateArg):
+    keyValueArray = templateArg.split('=')
+    if (len(keyValueArray) > 1):
+        return (keyValueArray[0], keyValueArray[1])
+    else:
+        return (keyValueArray[0], 'stdout')
+
+
+def _getJobConfigurationsFromArgs(args):
+    job = config.Job()
+    job.name = 'default'
+    for modelFile in args.models:
+        model = config.Model()
+        model.schema = modelFile
+        job.models.append(model)
+    templateParameters = _getTemplateParameters(args)
+
+    for templateFile in args.singleFileTemplates:
+        task = config.Task()
+        task.name = templateFile
+        task.singleFileTask = config.SingleFileTask()
+        (task.singleFileTask.template, task.singleFileTask.destFile) = _splitTemplateAndDestination(templateFile)
+        task.singleFileTask.templateParameters = templateParameters
+        job.tasks.append(task)
+
+    for templateFile in args.multiFileTemplates:
+        task = config.Task()
+        task.name = templateFile
+        task.multiFileTask = config.MultiFileTask()
+        (task.multiFileTask.template, task.multiFileTask.destDir) = _splitTemplateAndDestination(templateFile)
+        task.multiFileTask.templateParameters = templateParameters
+        job.tasks.append(task)
+    return [job]
+
+
+def getJobConfigurations(args):
+    """builds an list of code generation Jobs from the given command lines
+    and return it
+    """
+
+    if args.config is not None:
+        return _getJobConfigurationsFromConfigFile(args.config)
+    else:
+        return _getJobConfigurationsFromArgs(args)
+
+
+def _foundAllTemplates(codeGenerationJobs):
+    """checks up if all template file are accessible. For internal templates the
+    template file name is changed
+
+    returns True if all templates are available, else False
+    """
+
+    print('\nChecking up templates:')
+    foundAll = True
+    for job in codeGenerationJobs:
+        print('  template for job {}:'.format(job.name))
+        for task in job.tasks:
+            fileExists = False
+            if (task.singleFileTask is not None) and (task.singleFileTask.template is not None):                
+                (fileExists, task.singleFileTask.template) = _tryToFindTemplate(task.singleFileTask.template)
+            elif (task.multiFileTask is not None) and (task.multiFileTask.template is not None):
+                (fileExists, task.multiFileTask.template) = _tryToFindTemplate(task.multiFileTask.template)
+            if not fileExists:
+                foundAll = False
+    return foundAll
+
+
+def _tryToFindTemplate(templateFile):
+    """tests if the given file name is a external or an internal template. If it
+    is an internal template, then the file name is changed to a relative path.
+
+    Function return a tupel with the true or false as first element, and the file name
+    to the found file as second element
+    """
+
+    fileExists = False
+    templateFileToReturn = templateFile
+    if doesFileExist(templateFile):
+        fileExists = True
+    else:
+        internalTemplateName = 'yacg/generators/templates/{}.mako'.format(templateFile)
+        fileExists = doesFileExist(internalTemplateName)
+        templateFileToReturn = internalTemplateName
+    fileExistsString = getOkTxt('found') if fileExists else getErrorTxt('missing')
+    print('   {}\t{}'.format(fileExistsString, templateFile))
+    return (fileExists, templateFileToReturn)
+
+
+def _foundAllModels(codeGenerationJobs):
+    """checks up if all model file are accessible. For internal templates the
+    template file name is changed
+
+    returns True if all templates are available, else False
+    """
+
+    print('\nChecking up models:')
+    foundAll = True
+    for job in codeGenerationJobs:
+        print('  Models for job {}:'.format(job.name))
+        for model in job.models:
+            fileExists = doesFileExist(model.schema)
+            fileExistsString = getOkTxt('found') if fileExists \
+                else getErrorTxt('missing')
+            if not fileExists:
+                foundAll = False
+            print('   {}\t{}'.format(fileExistsString, model.schema))
+    return foundAll
+
+
+def _isConfigurationValid(codeGenerationJobs):
+    """checks up the give job configuration array and
+    returns True if valid else if not
+    """
+
+    isValid = True
+    if (codeGenerationJobs is None) or (len(codeGenerationJobs) == 0 ):
+        errorMsg = getErrorTxt('no generation jobs are given - cancel')
+        print(errorMsg)
+        return False
+    if _foundAllTemplates(codeGenerationJobs) is False:
+        isValid = False
+    if _foundAllModels(codeGenerationJobs) is False:
+        isValid = False
+    return isValid
+
+
 def main():
     """starts the program execution"""
     args = parser.parse_args()
-    argumentsAreOk = True
-    if not checkModelsToLoad(args):
-        argumentsAreOk = False
-    if not checkTemplatesToUse(args):
-        argumentsAreOk = False
-    if not argumentsAreOk:
-        printError('\nfound errors in configuration, cancel execution')
+    codeGenerationJobs = getJobConfigurations(args)
+    if not _isConfigurationValid(codeGenerationJobs):
         sys.exit(1)
-    loadedTypes = readModels(args)
-    templateParameters = getTemplateParameters(args)
-    i = 0
-    for template in args.templates:
-        internalTemplateName = 'generators/templates/{}.mako'.format(template)
-        isInternalTemplate = doesFileExist('yacg/{}'.format(internalTemplateName))
-        if isInternalTemplate is True:
-            templateFile = getInternalTemplatePath(internalTemplateName)
-            output = args.output[i]
-            renderSingleFileTemplate(loadedTypes, templateFile, output, templateParameters)
-        else:
-            printError('template not found: {}'.format(template))
-        i = i + 1
+    for job in codeGenerationJobs:
+        loadedTypes = readModels(job)
+        i = 0
+        for template in args.templates: # TODO remove args
+            internalTemplateName = 'generators/templates/{}.mako'.format(template)
+            isInternalTemplate = doesFileExist('yacg/{}'.format(internalTemplateName))
+            if isInternalTemplate is True:
+                templateFile = getInternalTemplatePath(internalTemplateName)
+                output = args.output[i] # TODO remove args
+                renderSingleFileTemplate(loadedTypes, templateFile, output, templateParameters)
+            else:
+                printError('template not found: {}'.format(template))
+            i = i + 1
 
 
 if __name__ == '__main__':
