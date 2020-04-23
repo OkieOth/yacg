fdc337febca77ebdba906e9f67774a3ba4e7f6b2
Okie Othsam <okie.othsam@gmail.com>
2020-04-23 22:33:21 +0200

    add config file handling

--- yacg.py
+++ yacg.py
@@ -1,6 +1,8 @@
 import argparse
 import sys
 import logging
+import json
+import yaml
 
 
 from yacg.util.fileUtils import doesFileExist
@@ -70,8 +72,18 @@ def _getTemplateParameters(args):
 
 
 def _getJobConfigurationsFromConfigFile(configFile):
-    # TODO
-    return []
+    if not doesFileExist(configFile):
+        printError('\ncan not find config file: {}'.format(configFile))
+        return []
+    if configFile.endswith('.json'):
+        with open(configFile) as config:
+            return json.load(config)
+    elif configFile.endswith('.yaml') or configFile.endswith('.yaml'):
+        with open(configFile) as config:
+            return yaml.load(config)
+    else:
+        printError('\nunknown config file extension, only json or yaml are supportted')
+        return []
 
 
 def _splitTemplateAndDestination(templateArg):
