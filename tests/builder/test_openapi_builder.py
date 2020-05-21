eaa647eee743b9631dff21b3dfda15775145a25f
Okie Othsam <okie.othsam@gmail.com>
2020-05-21 13:46:39 +0200

    improve tests for openApi parsing

--- tests/builder/test_openapi_builder.py
+++ tests/builder/test_openapi_builder.py
@@ -15,7 +15,29 @@ class TestOpenApiParsing (unittest.TestCase):
         self.assertIsNotNone(modelTypes)
         self.assertEqual(15, len(modelTypes))
         pathTypes = []
+        coreModelTypes = []
         for type in modelTypes:
             if isinstance(type, openapi.PathType):
                 pathTypes.append(type)
+            else:
+                coreModelTypes.append(type)
         self.assertEqual(4, len(pathTypes))
+        for path in pathTypes:
+            self.assertTrue(len(path.commands) > 0)
+            self.assertIsNotNone(path.pathPattern)
+            for command in path.commands:
+                self.assertTrue(len(command.tags) > 0)
+                self.assertTrue(
+                    (len(command.parameters) > 0) or
+                    (command.requestBody is not None))
+                for param in command.parameters:
+                    self.assertIsNotNone(param.name)
+                    self.assertIsNotNone(param.type)
+                if command.requestBody is not None:
+                    self.assertTrue(len(command.requestBody.content) > 0)
+                    for cont in command.requestBody.content:
+                        self.assertIsNotNone(cont.mimeType)
+                        self.assertIsNotNone(cont.type)
+
+                self.assertTrue(len(command.responses) > 0)
+
