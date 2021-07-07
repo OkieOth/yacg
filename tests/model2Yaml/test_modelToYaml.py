6ba8c8ae4284523fff5c13e09be087f3cc48eab6
Okie Othsam <okie.othsam@gmail.com>
2021-07-07 21:18:34 +0200

    fix test package name

--- tests/model2Yaml/test_modelToYaml.py
+++ tests/model2Yaml/test_modelToYaml.py
@@ -2,22 +2,22 @@ import unittest
 from pathlib import Path
 import shutil
 import os
-import modelToYaml
+from modelToYaml import trimModelFileName, convertModel
 from yacg.util.fileUtils import doesFileExist
 
 
 class TestModelToYaml (unittest.TestCase):
     def testTrimFileName(self):
-        ret = modelToYaml.trimModelFileName('test/test2/tttt.json')
+        ret = trimModelFileName('test/test2/tttt.json')
         self.assertEqual('tttt', ret)
-        ret = modelToYaml.trimModelFileName('test/test2/ttty.yaml')
+        ret = trimModelFileName('test/test2/ttty.yaml')
         self.assertEqual('ttty', ret)
-        ret = modelToYaml.trimModelFileName('test/test2/tttz.yaml')
+        ret = trimModelFileName('test/test2/tttz.yaml')
         self.assertFalse('ttty' == ret)
 
     def testDryRun(self):
         model = 'resources/models/json/yacg_config_schema.json'
-        modelToYaml.convertModel(model, True, 'dummy')
+        convertModel(model, True, 'dummy')
 
     def testConvertFile(self):
         dirpath = Path('tmp', 'model2yaml')
@@ -25,7 +25,7 @@ class TestModelToYaml (unittest.TestCase):
             shutil.rmtree(dirpath)
         os.mkdir(dirpath)
         model = 'resources/models/json/yacg_config_schema.json'
-        modelToYaml.convertModel(model, False, 'tmp/model2yaml')
+        convertModel(model, False, 'tmp/model2yaml')
         self.assertTrue(doesFileExist('tmp/model2yaml/yacg_config_schema.yaml'))
 
 if __name__ == '__main__':
