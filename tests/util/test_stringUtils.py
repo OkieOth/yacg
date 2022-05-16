90f3418a6389a553ad4521192742b6593872afd7
Stefan Gürtler <stefan.guertler@swarco.com>
2022-05-16 14:01:29 +0200

    Added some more string functions
    method to change snake case to camel case
    
    method to[Lower,Upper]Case

--- tests/util/test_stringUtils.py
+++ tests/util/test_stringUtils.py
@@ -1,7 +1,8 @@
 import unittest
 from yacg.util.stringUtils import toUpperCamelCase, toName
+from yacg.util.stringUtils import snakeToLowerCamelCase, snakeToUpperCamelCase, toLowerCase, toUpperCase
 
-
+# For executing these test run: pipenv run python3 -m unittest -v tests/util/test_stringUtils.py
 class TestStringUtils (unittest.TestCase):
     def testUpperCamelCase(self):
         t = toUpperCamelCase('i am a test')
@@ -26,3 +27,75 @@ class TestStringUtils (unittest.TestCase):
         self.assertNotEqual('a_1_', t)
         t = toName('a:1')
         self.assertEqual('a_1', t)
+
+    def testSnakeToLowerCamelCase(self):
+        t = snakeToLowerCamelCase(None)
+        self.assertEqual(None, t)
+
+        t = snakeToLowerCamelCase('')
+        self.assertEqual('', t)
+
+        t = snakeToLowerCamelCase('A')
+        self.assertEqual('a', t)
+
+        t = snakeToLowerCamelCase('this_is_a_snake_case')
+        self.assertEqual('thisIsASnakeCase', t)
+
+        t = snakeToLowerCamelCase('ALL_UPPER_SNAKE')
+        self.assertEqual('aLLUPPERSNAKE', t)
+
+    def testSnakeToUpperCamelCase(self):
+        t = snakeToUpperCamelCase(None)
+        self.assertEqual(None, t)
+
+        t = snakeToUpperCamelCase('')
+        self.assertEqual('', t)
+
+        t = snakeToUpperCamelCase('a')
+        self.assertEqual('A', t)
+
+        t = snakeToUpperCamelCase('this_is_a_snake_case')
+        self.assertEqual('ThisIsASnakeCase', t)
+
+        t = snakeToUpperCamelCase('ALL_UPPER_SNAKE')
+        self.assertEqual('ALLUPPERSNAKE', t)
+
+    def testToLowerCase(self):
+        # test text starting with upper case character
+        t = toLowerCase('AbCdE')
+        self.assertEqual('abCdE', t)
+        # test text starting with lower case character
+        t = toLowerCase('aBcDe')
+        self.assertEqual('aBcDe', t)
+        # test single lower case character
+        t = toLowerCase('x')
+        self.assertEqual('x', t)
+        # test single upper case character
+        t = toLowerCase('Y')
+        self.assertEqual('y', t)
+        # leading numbers prevent changing case of susequent letters
+        t = toLowerCase('1ABC')
+        self.assertEqual('1ABC', t)
+        # test empy string
+        t = toLowerCase('')
+        self.assertEqual('', t)
+
+    def testToUpperCase(self):
+        # test text starting with upper case character
+        t = toUpperCase('AbCdE')
+        self.assertEqual('AbCdE', t)
+        # test text starting with lower case character
+        t = toUpperCase('aBcDe')
+        self.assertEqual('ABcDe', t)
+        # test single lower case character
+        t = toUpperCase('x')
+        self.assertEqual('X', t)
+        # test single upper case character
+        t = toUpperCase('Y')
+        self.assertEqual('Y', t)
+        # leading numbers prevent changing case of susequent letters
+        t = toUpperCase('1abc')
+        self.assertEqual('1abc', t)
+        # test empy string
+        t = toUpperCase('')
+        self.assertEqual('', t)
