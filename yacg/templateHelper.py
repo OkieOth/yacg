3d4e9faa9e96186d028b6c9335871befc2921858
Stefan Gürtler <stefan.guertler@swarco.com>
2022-07-07 19:33:32 +0200

    drop old version of the function

--- yacg/templateHelper.py
+++ yacg/templateHelper.py
@@ -2,21 +2,6 @@
 """
 
 
-# def addLineBreakToDescription(textLine, indent=0):
-#     indentStr = ' ' * indent if indent > 0 else ''
-#     breakedText = ''
-#     currentLen = 0
-#     splittedLine = textLine.split()
-#     for t in splittedLine:
-#         if currentLen > 60:
-#             currentLen = 0
-#             breakedText = breakedText + '\n' + indentStr
-#         else:
-#             currentLen = currentLen + len(t)
-#             breakedText = breakedText + ' '
-#         breakedText = breakedText + t
-#     return breakedText
-
 def addLineBreakToDescription(textLine, indent=0, prefix=''):
     indentStr = (' ' * indent if indent > 0 else '') + prefix
     breakedText = prefix
